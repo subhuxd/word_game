121deea623f7426b2ec6511c29a111aed9607bc8
subhuu <subransunayak24@gmail.com>
2025-09-02 22:37:36 +0530

    game_3

--- word_game.py
+++ word_game.py
@@ -1,26 +1,4 @@
-# import random
-# name = input("what is your name?\n")
-# print("good luck",name)
-# words = ['ranbow','sky','river','moon','star','ocean','tree']
-# print(words)
-# turns = 3
-# while True:
-#     while turns > 0:
-#         word = random.choice(words)
-#         guess = input("guess the word")
-#         if guess == word:
-#             print("you win")
-#             break
-#         elif guess != word:
-#             turns -= 1
-#             print("try again!")
-#             print("you have",turns,"more guesses")
-#             if turns ==0:
-#                 print("you lose! the word is", word)
-#          play_again = input("Do you want to continue the game? (yes/no): ").lower()
-#     if play_again != "yes":
-#         print("Thanks for playing!")
-#         break
+
 
 import random
 name = input("what is your name?\n")
